d0098188195c1fd391c52d24a17e6f7326534470
pirates19 <nicholas.ellul1@gmail.com>
2015-11-19 14:46:54 -0500

    Done

---
+++ TwoDimensionalArray505/TwoDimesionalArrayForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TwoDimensionalArray505
+{
+    partial class frm2DArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.nudLength = new System.Windows.Forms.NumericUpDown();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.lstListOfNumbers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(74, 196);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(126, 38);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Generate and Find Average";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // nudLength
+            // 
+            this.nudLength.Location = new System.Drawing.Point(31, 161);
+            this.nudLength.Name = "nudLength";
+            this.nudLength.Size = new System.Drawing.Size(69, 20);
+            this.nudLength.TabIndex = 1;
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(176, 161);
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(69, 20);
+            this.nudWidth.TabIndex = 2;
+            // 
+            // lstListOfNumbers
+            // 
+            this.lstListOfNumbers.FormattingEnabled = true;
+            this.lstListOfNumbers.Location = new System.Drawing.Point(74, 29);
+            this.lstListOfNumbers.Name = "lstListOfNumbers";
+            this.lstListOfNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstListOfNumbers.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Width";
+            // 
+            // frm2DArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstListOfNumbers);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.nudLength);
+            this.Controls.Add(this.btnGo);
+            this.Name = "frm2DArray";
+            this.Text = "Array Average";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.NumericUpDown nudLength;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.ListBox lstListOfNumbers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TwoDimensionalArray505/TwoDimesionalArrayForm.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Created by: Nicholas Ellul
+ * Created on: 19-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 5-06
+ * This program returns the average of a 2D array.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoDimensionalArray505
+{
+    public partial class frm2DArray : Form
+    {
+        Random randomizedNum = new Random();
+        public frm2DArray()
+        {
+            InitializeComponent();
+        }
+
+
+       public int AverageOf2DArray(int[,] theArray2, int width, int length)
+       {
+            //returns average of a two dimensional array
+            int sum = 0;
+            int elementsInArray = width * length;
+
+          for (int columnCounter = 0; columnCounter < length; columnCounter++)
+          {
+                for (int rowCounter = 0; rowCounter < width; rowCounter++)
+                {
+                    sum = sum + theArray2[columnCounter, rowCounter];
+                }
+          }
+            
+          if(elementsInArray == 0)
+            {
+                return 0;
+            }
+
+             return sum/elementsInArray;
+      }
+
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //input
+            int length = Convert.ToInt32(nudLength.Value);
+            int width = Convert.ToInt32(nudWidth.Value);
+            int generatedNumber;
+            string theAverage;
+
+            int[,] my2Darray = new int[length, width];
+
+            //process
+            lstListOfNumbers.Items.Clear();
+            for(int lengthCounter = 0; lengthCounter < length; lengthCounter++)
+            {
+                for(int widthCounter = 0; widthCounter < width; widthCounter++)
+                {
+                    generatedNumber = randomizedNum.Next(0, 10 + 1);
+                    my2Darray[lengthCounter, widthCounter] = generatedNumber;
+                    this.lstListOfNumbers.Items.Add(generatedNumber);
+                }
+            }
+
+
+            //output
+            theAverage = Convert.ToString(AverageOf2DArray(my2Darray, width, length));
+            MessageBox.Show(theAverage, "Your Average is...");
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+          /*  
+            // generate the array
+            int width;
+            int length;
+            Random randomNumberGenerator = new Random();
+            int aRandomNumber;
+            string aPieceOfText = null;
+
+            width = 4; //Convert.ToInt16(this.nudWidth.Value);
+            length = 2;//Convert.ToInt16(this.nudLength.Value);
+
+            // declare the array
+            int[,] a2DArray = new int[width, length];
+
+            for (int widthCounter = 0; widthCounter < width; widthCounter++)
+            {
+                for (int lengthCounter = 0; lengthCounter < length; lengthCounter++)
+                {
+                    aRandomNumber = randomNumberGenerator.Next(0, 9 + 1);
+                    a2DArray[widthCounter, lengthCounter] = aRandomNumber;
+                    aPieceOfText = aPieceOfText + " " + aRandomNumber;
+                }
+                    aPieceOfText = aPieceOfText + "\r" + "\n";
+            }
+
+            //this.txtArray.Text = aPieceOfText;
+            MessageBox.Show(aPieceOfText); */
+        }
+       
+    }
+}
